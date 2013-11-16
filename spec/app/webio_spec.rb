64b25b9d31fa5b849e8b46275d03bffe0fb655f6
Yoshihiro Kameda <kameda.sbng@gmail.com>
2013-11-17 00:36:27 +0900

    refactoring

--- spec/app/webio_spec.rb
+++ spec/app/webio_spec.rb
@@ -6,11 +6,11 @@ describe WebIO do
 
   context :gets do
     before(:each) do
-      @input_line = nil
+      @response = nil
       @webio_thread = Thread.new do
         @webio = WebIO.new('0.0.0.0:53333')
         while l=@webio.gets
-          @input_line = l
+          @response = 'koge' + l
         end
       end
       sleep 0.5 # getsを呼ぶまで待つ
@@ -22,12 +22,16 @@ describe WebIO do
     end
 
     def send_url(command)
-      `curl http://localhost:53333/hoge`
+      `curl http://localhost:53333/#{command}`
+    end
+
+    def response_for(command)
+      send_url(command)
+      @response
     end
 
     it 'receive web url parameter' do
-      `curl http://localhost:53333/hoge`
-      @input_line.should eq 'hoge'
+      response_for('hoge').should eq 'kogehoge'
     end
   end
 
